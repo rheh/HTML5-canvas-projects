1615ef69aa2a29056666711908caf7d9d412e8b6
Ray Hammond <ray@keyzo.co.uk>
2019-02-03 21:48:03 +0000

    Fixed issued with led scroller not loading in ray github

--- led_scroller/led_scroller.js
+++ led_scroller/led_scroller.js
@@ -1,112 +1,118 @@
-var letters_pink = null, 
-	letters_blue = null, 
-	sMessage = "", 
-	canvas = null, 
-	ctx = null, 
-	LETTER_HEIGHT = 343, 
-	LETTER_WIDTH = 267, 
-	iStep = 0, 
-	iDrawPhase = 0, 
-	job = null, 
-	bPink = true;
+(function (window) {
+	var letters_pink = null,
+		letters_blue = null,
+		sMessage = "",
+		canvas = null,
+		ctx = null,
+		LETTER_HEIGHT = 343,
+		LETTER_WIDTH = 267,
+		iStep = 0,
+		iDrawPhase = 0,
+		job = null,
+		bPink = true;
 
-function drawLetter(iSpriteRow, iSpriteCol, iPos) {
+	let document = window.document;
 
-	var xPos = (LETTER_WIDTH * iPos) - iStep;
+	function drawLetter(iSpriteRow, iSpriteCol, iPos) {
 
-	if ((xPos > 0 - LETTER_WIDTH) && (xPos < 1200 + LETTER_WIDTH)) {
+		var xPos = (LETTER_WIDTH * iPos) - iStep;
 
-		bPink = document.getElementById('pink').checked;
-		
-		if (bPink === true) {
+		if ((xPos > 0 - LETTER_WIDTH) && (xPos < 1200 + LETTER_WIDTH)) {
 
-			ctx.drawImage(letters_pink, iSpriteRow * LETTER_WIDTH, iSpriteCol * LETTER_HEIGHT, LETTER_WIDTH, LETTER_HEIGHT, xPos, 0, LETTER_WIDTH, LETTER_HEIGHT);
+			bPink = document.getElementById('pink').checked;
 
-		} else {
+			if (bPink === true) {
 
-			ctx.drawImage(letters_blue, iSpriteRow * LETTER_WIDTH, iSpriteCol * LETTER_HEIGHT, LETTER_WIDTH, LETTER_HEIGHT, xPos, 0, LETTER_WIDTH, LETTER_HEIGHT);
+				ctx.drawImage(letters_pink, iSpriteRow * LETTER_WIDTH, iSpriteCol * LETTER_HEIGHT, LETTER_WIDTH, LETTER_HEIGHT, xPos, 0, LETTER_WIDTH, LETTER_HEIGHT);
+
+			} else {
+
+				ctx.drawImage(letters_blue, iSpriteRow * LETTER_WIDTH, iSpriteCol * LETTER_HEIGHT, LETTER_WIDTH, LETTER_HEIGHT, xPos, 0, LETTER_WIDTH, LETTER_HEIGHT);
+
+			}
 
 		}
 
 	}
 
-}
+	function draw() {
 
-function draw() {
+		var iCounter = 0,
+			iCharCode = 0;
 
-	var iCounter = 0, 
-		iCharCode = 0;
+		for (iCounter = 0; iCounter < sMessage.length; iCounter++) {
 
-	for ( iCounter = 0; iCounter < sMessage.length; iCounter++) {
+			iCharCode = sMessage.charCodeAt(iCounter);
 
-		iCharCode = sMessage.charCodeAt(iCounter);
+			if (iCharCode > 64 && iCharCode < 91) {
+				iSpriteCol = Math.abs(65 - iCharCode) % 5;
+				iSpriteRow = Math.floor(Math.abs(65 - iCharCode) / 5);
+			} else {
+				iSpriteCol = 1;
+				iSpriteRow = 5;
+			}
 
-		if (iCharCode > 64 && iCharCode < 91) {
-			iSpriteCol = Math.abs(65 - iCharCode) % 5;
-			iSpriteRow = Math.floor(Math.abs(65 - iCharCode) / 5);
-		} else {
-			iSpriteCol = 1;
-			iSpriteRow = 5;
+			drawLetter(iSpriteCol, iSpriteRow, iCounter);
 		}
 
-		drawLetter(iSpriteCol, iSpriteRow, iCounter);
+		iSpriteCol = 1;
+		iSpriteRow = 5;
+
+		for (iCounter; iCounter < sMessage.length + 10; iCounter++) {
+
+			drawLetter(iSpriteCol, iSpriteRow, iCounter);
+		}
+
+		iDrawPhase += 1;
+		iStopPoint = (27 * sMessage.length);
+
+		if (iDrawPhase < iStopPoint) {
+			iStep += 10;
+		} else {
+			clearInterval(job);
+		}
+
 	}
 
-	iSpriteCol = 1;
-	iSpriteRow = 5;
+	function startAnim(document) {
 
-	for (iCounter; iCounter < sMessage.length + 10; iCounter++) {
-
-		drawLetter(iSpriteCol, iSpriteRow, iCounter);
-	}
-
-	iDrawPhase += 1;
-	iStopPoint = (27 * sMessage.length);
-
-	if (iDrawPhase < iStopPoint) {
-		iStep += 10;
-	} else {
 		clearInterval(job);
+		sMessage = document.getElementById('text').value.toUpperCase();
+		iDrawPhase = 0;
+		iStep = 0;
+
+		if (sMessage.length === 0) {
+			sMessage = "Please enter a phrase";
+			document.getElementById('text').value = sMessage;
+		} else {
+
+			// Add 5 spaces padding so the text start off right
+			sMessage = "     " + sMessage;
+			// Start the timer
+			job = setInterval(draw, 10);
+		}
 	}
 
-}
+	function init() {
 
-function startAnim() {
+		// Grab the clock element
+		canvas = document.getElementById('led');
 
-	clearInterval(job);
-	sMessage = document.getElementById('text').value.toUpperCase();
-	iDrawPhase = 0;
-	iStep = 0;
+		// Canvas supported?
+		if (canvas.getContext('2d')) {
+			ctx = canvas.getContext('2d');
 
-	if (sMessage.length === 0) {
-		sMessage = "Please enter a phrase";
-		document.getElementById('text').value = sMessage;
-	} else {
+			letters_blue = new Image();
+			letters_blue.src = 'letters-blue.jpg?v=10';
 
-		// Add 5 spaces padding so the text start off right
-		sMessage = "     " + sMessage;
-		// Start the timer
-		job = setInterval(draw, 10);
+			letters_pink = new Image();
+			letters_pink.src = 'letters-pink.jpg?v=10';
+			letters_pink.onload = startAnim.bind(this, document);
+
+		} else {
+			alert("Canvas not supported!");
+		}
 	}
-}
 
-function init() {
-
-	// Grab the clock element
-	canvas = document.getElementById('led');
-
-	// Canvas supported?
-	if (canvas.getContext('2d')) {
-		ctx = canvas.getContext('2d');
-
-		letters_blue = new Image();
-		letters_blue.src = 'letters-blue.jpg?v=10';
-
-		letters_pink = new Image();
-		letters_pink.src = 'letters-pink.jpg?v=10';
-		letters_pink.onload = startAnim;
-
-	} else {
-		alert("Canvas not supported!");
-	}
-}
+	init();
+})(window);
